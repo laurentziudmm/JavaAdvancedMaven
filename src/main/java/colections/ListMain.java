264e2dec8f9cf94cb9e7a002f2ae5961df759df4
laurentziudmm <laurentziudmm@gmail.com>
2020-02-09 13:09:01 +0200

    update listMain

--- src/main/java/colections/ListMain.java
+++ src/main/java/colections/ListMain.java
@@ -7,6 +7,8 @@ import java.util.List;
 public class ListMain {
     public static void main(String[] args) {
 
+        //explain ArrayList
+        //@mention
         List<Integer> listInteger = new ArrayList();
 
         listInteger.add(1);
