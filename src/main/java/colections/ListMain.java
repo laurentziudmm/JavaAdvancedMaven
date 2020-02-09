6991cc12c87a333e6971a0c76811e529d72b3604
laurentziudmm <laurentziudmm@gmail.com>
2020-02-09 12:28:16 +0200

    added list ArrayList LinkedList

---
+++ src/main/java/colections/ListMain.java
@@ -0,0 +1,24 @@
+package colections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListMain {
+    public static void main(String[] args) {
+
+        List<Integer> listInteger = new ArrayList();
+
+        listInteger.add(1);
+        listInteger.add(5);
+        listInteger.add(3);
+        listInteger.add(2);
+
+        System.out.println(listInteger);
+
+        System.out.println(listInteger.contains(7));
+
+        listInteger.add(0,7);
+        System.out.println(listInteger);
+
+    }
+}
