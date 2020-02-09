87b9c768695eb9fa712a07c66e542392aad937a8
laurentziudmm <laurentziudmm@gmail.com>
2020-02-09 15:39:58 +0200

    update products

--- src/main/java/products/Main.java
+++ src/main/java/products/Main.java
@@ -12,8 +12,18 @@ public class Main {
         products.add(new Product("Ciocolata",3.7,100.5));
         products.add(new Product("Bomboana",33.7,14.5));
         System.out.println(products.toString());
+        System.out.println();
 
         Collections.sort(products);
+
+        System.out.println(products.contains(new Product("Bomboana",33.7,14.5)));
+        System.out.println();
+
+        products.remove(new Product("Bomboana",33.7,14.5));
+
+        System.out.println(products);
+        System.out.println();
+
 
     }
 }
