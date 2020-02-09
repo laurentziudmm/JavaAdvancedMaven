75dee941365cd7c828868a7095a1b29d45c0fcff
laurentziudmm <laurentziudmm@gmail.com>
2020-02-09 15:55:55 +0200

    update set products linked hash set

--- src/main/java/products/SetMain.java
+++ src/main/java/products/SetMain.java
@@ -1,12 +1,13 @@
 package products;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class SetMain {
     public static void main(String[] args) {
 
-        Set<Product> setProducts = new HashSet<Product>();
+        Set<Product> setProducts = new LinkedHashSet<Product>();
 
         setProducts.add(new Product("Apa",3.7,10.5));
         setProducts.add(new Product("Bomboana",33.7,14.5));
