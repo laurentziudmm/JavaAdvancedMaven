76f560ec4240a789c2c7e26ce8de016c81782961
laurentziudmm <laurentziudmm@gmail.com>
2020-02-09 13:58:11 +0200

    updated listMain

--- src/main/java/colections/ListMain.java
+++ src/main/java/colections/ListMain.java
@@ -2,12 +2,13 @@ package colections;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ListMain {
     public static void main(String[] args) {
 
-        List<Integer> listInteger = new ArrayList();
+        List<Integer> listInteger = new LinkedList<Integer>();
 
         listInteger.add(1);
         listInteger.add(5);
@@ -23,6 +24,29 @@ public class ListMain {
 
         Collections.sort(listInteger);
         System.out.println(listInteger);
+
+        listInteger.remove(0);
+        System.out.println(listInteger);
+
+
+        System.out.println();
+        List<Integer> listInteger2 = new ArrayList<Integer>();
+        listInteger2.add(100);
+        listInteger2.addAll(listInteger);
+        System.out.println(listInteger2);
+
+
+        List<Integer> numereImpare = new LinkedList<Integer>();
+        for (int i = 0; i < 100 ; i++) {
+
+            if (i % 2 != 0) {
+
+                numereImpare.add(i);
+            }
+        }
+
+        System.out.println(numereImpare);
+
 
     }
 }
