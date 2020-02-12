7b80673358f45d86ad2e28311d0fb8d95b246d69
laurentziudmm <laurentziudmm@gmail.com>
2020-02-12 10:00:51 +0200

    List 1 finished

--- src/main/java/homeWork/Main.java
+++ src/main/java/homeWork/Main.java
@@ -1,19 +1,28 @@
 package homeWork;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) {
 
         /**
          * Lists with strings:
          *
-         * 1. Create an ArrayList list1 with strings and add the following values: Green, Red, Blue.
+         * 1. Create an ArrayList list1 with strings
+         * and add the following values: Green, Red, Blue.
          * 2. Add value Yellow on the first position of the list.
          *
          * 3. Print the list.
+         *
          * 4. Print the size of the list.
+         *
          * 5. Sort the list using Collections.sort() method.
+         *
          * 6. Print the list.
+         *
          * 7. Check if value Blue is contained in the list and print the result.
+         *
          * 8. Create another ArrayList list2 with strings and add the following values: White, Black.
          * 9. Check if list1 contains list2 - use method containsAll(). Print the result.
          * 10. Add list2 to list1 - use method addAll().
@@ -22,6 +31,28 @@ public class Main {
          * 13. Iterate over list1 and print each value on a new line. Use Method2 from  https://www.geeksforgeeks.org/iterating-arraylists-java/.
          * 14. What if instead of ArrayList in above exercises we use LinkedList? Is it still going to work the same?
          */
+
+
+        ArrayList<String> list1 = new ArrayList<String>();
+        list1.add("Green");
+        list1.add("Red");
+        list1.add("Blue");
+        list1.add(0,"Yellow");
+        System.out.println("Print the list \n");
+        System.out.println(list1+"\n");
+
+        System.out.println("Print the size of the list\n");
+        int size = list1.size();
+        System.out.println(size + "\n");
+
+        Collections.sort(list1);
+        System.out.println("Collections Sort Print List \n");
+        System.out.println(list1 + "\n");
+
+        System.out.println("list1 Contains Blue \n");
+        System.out.println(list1.contains("Blue") + "\n");
+
+
 
 
 
