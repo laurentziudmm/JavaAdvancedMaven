ba065405d2b954089ce434acebb03e08025a8aad
laurentziudmm <laurentziudmm@gmail.com>
2020-02-12 09:48:13 +0200

    Added HomeWork

---
+++ src/main/java/homeWork/Main.java
@@ -0,0 +1,29 @@
+package homeWork;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /**
+         * Lists with strings:
+         *
+         * 1. Create an ArrayList list1 with strings and add the following values: Green, Red, Blue.
+         * 2. Add value Yellow on the first position of the list.
+         *
+         * 3. Print the list.
+         * 4. Print the size of the list.
+         * 5. Sort the list using Collections.sort() method.
+         * 6. Print the list.
+         * 7. Check if value Blue is contained in the list and print the result.
+         * 8. Create another ArrayList list2 with strings and add the following values: White, Black.
+         * 9. Check if list1 contains list2 - use method containsAll(). Print the result.
+         * 10. Add list2 to list1 - use method addAll().
+         * 11. Print list1.
+         * 12. Remove value Red from list1. Print the list1.
+         * 13. Iterate over list1 and print each value on a new line. Use Method2 from  https://www.geeksforgeeks.org/iterating-arraylists-java/.
+         * 14. What if instead of ArrayList in above exercises we use LinkedList? Is it still going to work the same?
+         */
+
+
+
+    }
+}
