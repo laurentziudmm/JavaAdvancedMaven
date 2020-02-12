1bcf2dd12aca3240b853f7c57bf2877901942c04
laurentziudmm <laurentziudmm@gmail.com>
2020-02-12 10:16:58 +0200

    Added Set with Strings Homework

---
+++ src/main/java/homeWork/SetMain.java
@@ -0,0 +1,25 @@
+package homeWork;
+
+public class SetMain {
+    public static void main(String[] args) {
+
+        /**
+         * Set with strings:
+         * 1. Create a HashSet set1 with strings and add the following values: Green, Red, Blue.
+         * 2. Print the set.
+         * 3. Add values Yellow, Green to the set.
+         * 4. Print the set. Why does it contain only one value of Green?
+         * 5. Print the size of the set.
+         * 6. Check if value Blue is contained in the list and print the result.
+         * 7. Create another HashSet set2 with strings and add the following values: White, Black.
+         * 8. Check if set1 contains set2 - use method containsAll(). Print the result.
+         * 9. Add set2 to set1 - use method addAll().
+         * 10. Print set1.
+         * 11. Remove value Red from set1. Print the set1.
+         * 12. Iterate over set1 and print each value on a new line. Use Method2 from  https://www.geeksforgeeks.org/iterating-arraylists-java/.
+         * 13. What if instead of HashSet in above exercises we use LinkedHashSet? Is it still going to work the same?
+         */
+
+
+    }
+}
