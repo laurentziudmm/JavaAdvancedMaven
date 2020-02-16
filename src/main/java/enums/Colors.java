72c9ab62fd1627197f54b98d46758c21b63fa4df
laurentziudmm <laurentziudmm@gmail.com>
2020-02-16 10:13:43 +0200

    Enum Example 2

--- src/main/java/enums/Colors.java
+++ src/main/java/enums/Colors.java
@@ -1,4 +1,22 @@
 package enums;
 
-public enum rgb {
+public enum Colors {
+    RED("#FF0000",31),
+    BLUE("#0000FF",33),
+    GREEN("#00FF00",34);
+
+    private String colorCode;
+    private Integer transparency;
+
+    Colors(String colorCode,Integer transparency) {
+        this.transparency = transparency;
+        this.colorCode = colorCode;
+    }
+
+    public String getColorCode() {
+        return colorCode;
+    }
+    public Integer getTransparency() {
+        return transparency;
+    }
 }
--- src/main/java/enums/MainColors.java
+++ src/main/java/enums/MainColors.java
@@ -1,4 +1,22 @@
 package enums;
 
 public class MainColors {
+    public static void main(String[] args) {
+
+//        Colors[] colors = Colors.values();
+//
+//        for (Colors colors1 : colors) {
+//            System.out.println(colors1);
+//        }
+
+        System.out.println(Colors.RED.getColorCode());
+        System.out.println(Colors.values());
+
+        for (Colors colors1 : Colors.values()) {
+            System.out.println(colors1.name()+" "+colors1.getColorCode());
+        }
+
+        System.out.println(Colors.valueOf("RED"));
+
+    }
 }
