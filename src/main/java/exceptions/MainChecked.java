45ae9e969ffd074cddd4d8776355665fb861a0d7
laurentziudmm <laurentziudmm@gmail.com>
2020-02-16 11:05:41 +0200

    Exceptions Checked

--- src/main/java/exceptions/MainChecked.java
+++ src/main/java/exceptions/MainChecked.java
@@ -1,4 +1,24 @@
 package exceptions;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class MainChecked {
+    public static void main(String[] args) {
+
+        try {
+            readFile();
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Programul merge mai departe");
+
+    }
+
+    public static void readFile() throws FileNotFoundException {
+
+        FileReader file = new FileReader("C:\\test\\a.txt");
+
+
+    }
 }
