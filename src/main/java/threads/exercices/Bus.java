01da649c148ede9356e3874652ddce35bcee6215
laurentziudmm <laurentziudmm@gmail.com>
2020-02-17 08:17:23 +0200

    Bus update

--- src/main/java/threads/exercices/Bus.java
+++ src/main/java/threads/exercices/Bus.java
@@ -3,11 +3,15 @@ package threads.exercices;
 public class Bus {
     public static void main(String[] args) {
 
-        Bench bench = new Bench(1);
-        OldLady lady = new OldLady(bench,"Mariora");
+        Bench bench = new Bench(5);
+        OldLady lady1 = new OldLady(bench,"Mariora");
         OldLady lady2 = new OldLady(bench,"Dumitrita");
-        lady.start();
+        OldLady lady3 = new OldLady(bench,"Thereza");
+        OldLady lady4 = new OldLady(bench,"Saveta");
+        lady1.start();
         lady2.start();
+        lady3.start();
+        lady4.start();
 
 
     }
